b85df3450ad019f897a70a3f74480e01dc5ab459
Шайдуллин Ильдар <i.shaidullin@soccard.ru>
2025-08-18 10:43:51 +0300

    hw06 Концепты проектирования ООП. ДЗ: Эмулятор банкомата. Замечания к PR.

--- L12-solid/src/main/java/ru/otus/l12/homework/exception/ATMException.java
+++ L12-solid/src/main/java/ru/otus/l12/homework/exception/ATMException.java
@@ -1,6 +1,6 @@
 package ru.otus.l12.homework.exception;
 
-public class ATMException extends Exception {
+public class ATMException extends RuntimeException {
 
     public ATMException(String message) {
         super(message);
