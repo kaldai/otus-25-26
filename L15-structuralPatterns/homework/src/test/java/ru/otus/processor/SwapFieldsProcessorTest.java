d94032756d573d69fb3c11ed26891a52c53eec2f
Шайдуллин Ильдар <i.shaidullin@soccard.ru>
2025-09-10 23:04:29 +0300

    hw07 Structural patterns. ДЗ: Обработчик сообщений.

--- L15-structuralPatterns/homework/src/test/java/ru/otus/processor/SwapFieldsProcessorTest.java
+++ L15-structuralPatterns/homework/src/test/java/ru/otus/processor/SwapFieldsProcessorTest.java
@@ -1,14 +1,11 @@
 package ru.otus.processor;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-import java.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import ru.otus.model.Message;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
 class SwapFieldsProcessorTest {
@@ -26,27 +23,5 @@ class SwapFieldsProcessorTest {
         // Assert
         assertEquals("value12", result.getField11());
         assertEquals("value11", result.getField12());
-    }
-
-    @Test
-    void testEvenSecondExceptionProcessor_OddSecond_NoException() {
-        // Arrange
-        var fixedTime = LocalDateTime.of(2023, 1, 1, 12, 0, 1); // Нечетная секунда
-        var processor = new EvenSecondExceptionProcessor(() -> fixedTime);
-        var message = Message.builder(1L).build();
-
-        // Act & Assert
-        assertDoesNotThrow(() -> processor.process(message));
-    }
-
-    @Test
-    void testEvenSecondExceptionProcessor_EvenSecond_ThrowsException() {
-        // Arrange
-        var fixedTime = LocalDateTime.of(2023, 1, 1, 12, 0, 2); // Четная секунда
-        var processor = new EvenSecondExceptionProcessor(() -> fixedTime);
-        var message = Message.builder(1L).build();
-
-        // Act & Assert
-        assertThrows(RuntimeException.class, () -> processor.process(message));
     }
 }
